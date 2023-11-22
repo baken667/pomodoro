6756d9f29226580495468f8233a38c98535cfd30
baken.wws <baken.wws@gmail.com>
2023-11-22 07:04:53 +0500

    add target blank to footer links

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -2,8 +2,8 @@
   <footer class="footer footer-center p-10 bg-primary text-primary-content gap-6">
     <aside>
       <p class="font-bold flex flex-row gap-3">
-        <a class="hover:link" href="https://instagram.com/catstore404">Instagram</a>
-        <a class="hover:link" href="https://github.com/baken667/pomodoro">GitHub</a>
+        <a class="hover:link" href="https://instagram.com/catstore404" target="_blank">Instagram</a>
+        <a class="hover:link" href="https://github.com/baken667/pomodoro" target="_blank">GitHub</a>
       </p>
     </aside>
   </footer>
